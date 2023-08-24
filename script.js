52cc66779836bdbeb63ce5e094e259ec92b9a0ac
yuhang zhu <zhucolley@outlook.com>
2023-08-23 21:21:25 -0400

    acheive same result with much easier to understand code -- refactor the code

--- script.js
+++ script.js
@@ -7,35 +7,33 @@ window.addEventListener("load", () => {
 h5s.forEach((h5) => {
   h5.addEventListener("click", (e) => {
     const sibling = e.currentTarget.nextElementSibling;
-    const nextElement = e.currentTarget.nextSibling;
+    // const nextElement = e.currentTarget.nextSibling;
     const arrow = e.currentTarget.querySelector("img");
 
     if (e.currentTarget.classList.contains("bold-heading")) {
-      addHideClassFromAllSpans();
-      removeRotateClassFromAllH5s();
+      sibling.classList.toggle("hide");
+      arrow.classList.toggle("rotate");
+      e.currentTarget.classList.toggle("bold-heading");
       return;
     }
     addHideClassFromAllSpans();
     removeRotateClassFromAllH5s();
-    sibling.classList.remove("hide");
-    arrow.classList.add("rotate");
-    e.currentTarget.classList.add("bold-heading");
+
+    sibling.classList.toggle("hide");
+    arrow.classList.toggle("rotate");
+    e.currentTarget.classList.toggle("bold-heading");
   });
 });
 
 function addHideClassFromAllSpans() {
   spans.forEach((span) => {
-    if (!span.classList.contains("hide")) {
-      span.classList.add("hide");
-    }
+    span.classList.add("hide");
   });
 }
 function removeRotateClassFromAllH5s() {
   h5s.forEach((h5) => {
     const img = h5.querySelector("img");
-    if (img.classList.contains("rotate")) {
-      img.classList.remove("rotate");
-      h5.classList.remove("bold-heading");
-    }
+    img.classList.remove("rotate");
+    h5.classList.remove("bold-heading");
   });
 }
